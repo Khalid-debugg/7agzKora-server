3421a94468ed231707aec1117857027941664793
Khalid-debugg <khalidsherif.dev@gmail.com>
2024-02-17 11:02:36 +0200

    Setup MikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.fork({}).create(Post, {
+    title: "my fift post",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  await orm.em.persistAndFlush(post);
+};
+main();
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { Migrator } from "@mikro-orm/migrations";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { defineConfig, PostgreSqlDriver } from "@mikro-orm/postgresql";
+import path from "path";
+export default defineConfig({
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    glob: "!(*.d).{js,ts}", // how to match migration files (all .js and .ts files, but not .d.ts)
+  },
+  entities: [Post],
+  dbName: "7agzKora",
+  password: "shefa2130",
+  extensions: [Migrator],
+  driver: PostgreSqlDriver,
+  debug: !__prod__,
+  allowGlobalContext: true,
+});
